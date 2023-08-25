7dbf98b85cac95b35847bf3e06055d58af7250e5
wfranke <wfranke@DESKTOP-1GDPVQA>
2023-08-25 10:07:19 -0500

    saving changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   root 'homepage#index'
 
   # Add routes below this line
-
+  get 'tasks' => 'tasks#index'
   # Add routes below above line
-
+  post 'tasks' => 'tasks#create'
   # Redirect all other paths to index page, which will be taken over by AngularJS
+  delete 'tasks/:id' => 'tasks#destroy'
+
+  put '/tasks/:id/mark_complete' => 'tasks#mark_complete'
+
+  put '/tasks/:id/mark_active'   => 'tasks#mark_active'
+
   get '*path' => 'homepage#index'
 end
